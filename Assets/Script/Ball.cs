6dc739153a81270918bdd4177439047ac2757c04
NemoKingFish <anakornduangjai@gmail.com>
2025-03-30 15:40:04 +0700

    Effect

--- Assets/Script/Ball.cs
+++ Assets/Script/Ball.cs
@@ -5,6 +5,12 @@ public class Ball : MonoBehaviour
     public float speed = 5f;
     private Rigidbody rb;
 
+    public GameObject hitPrefabScore0;
+    public GameObject hitPrefabScore1;
+    public GameObject hitPrefabScore3;
+    public GameObject hitPrefabScore5;
+    public GameObject hitPrefabPajinkoBall;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -17,14 +23,57 @@ public class Ball : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Paddle"))
         {
-                        float hitFactor = (transform.position.x - collision.transform.position.x) / (collision.collider.bounds.size.x / 2);
+            float hitFactor = (transform.position.x - collision.transform.position.x) / (collision.collider.bounds.size.x / 2);
             Vector3 newDirection = new Vector3(hitFactor, 0, 1).normalized;
             rb.linearVelocity = newDirection * speed;
         }
         else
         {
-                        Vector3 reflectedVelocity = Vector3.Reflect(rb.linearVelocity, collision.contacts[0].normal);
+            Vector3 reflectedVelocity = Vector3.Reflect(rb.linearVelocity, collision.contacts[0].normal);
             rb.linearVelocity = reflectedVelocity;
         }
+
+        if (collision.gameObject.name == "0 Score")
+        {
+            if (hitPrefabScore0 != null)
+            {
+                GameObject effect = Instantiate(hitPrefabScore0, transform.position, Quaternion.identity);
+                Destroy(effect, 1f);
+            }
+        }
+        if (collision.gameObject.name == "1 Score" || collision.gameObject.name == "1 Score (1)")
+        {
+            if (hitPrefabScore1 != null)
+            {
+                GameObject effect = Instantiate(hitPrefabScore1, transform.position, Quaternion.identity);
+                Destroy(effect, 1f);
+            }
+        }
+        if (collision.gameObject.name == "3 Score" || collision.gameObject.name == "3 Score (1)")
+        {
+            if (hitPrefabScore3 != null)
+            {
+                GameObject effect = Instantiate(hitPrefabScore3, transform.position, Quaternion.identity);
+                Destroy(effect, 1f);
+            }
+        }
+        if (collision.gameObject.name == "5 Score" || collision.gameObject.name == "5 Score (1)")
+        {
+            if (hitPrefabScore5 != null)
+            {
+                GameObject effect = Instantiate(hitPrefabScore5, transform.position, Quaternion.identity);
+                Destroy(effect, 1f);
+            }
+        }
+        if (collision.gameObject.name == "PajinkoBall" || collision.gameObject.name == "PajinkoBall(Clone)")
+        {
+            if (hitPrefabScore5 != null)
+            {
+                GameObject effect = Instantiate(hitPrefabPajinkoBall, transform.position, Quaternion.identity);
+                Destroy(effect, 0.5f);
+            }
+        }
+
+        
     }
 }
